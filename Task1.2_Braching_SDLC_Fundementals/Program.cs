c27e588d780f5e9bfe61fe0d2ebc737741571255
hamza <adawihsa@gmail.com>
2026-08-01 03:47:33 +0400

    Added 2 Comment Lines

---
+++ Task1.2_Braching_SDLC_Fundementals/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task1._2_Braching_SDLC_Fundementals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this project will be in new branch named (b1) it will not be deleted ");
+            Console.WriteLine("");
+        }
+    }
+}

f487b0b0d6a67ec30c331683a19acdde88d0a7da
hamza <adawihsa@gmail.com>
2026-08-01 03:44:20 +0400

    Task 1.1 - GitHub Project Push and know how to use IDE

---
+++ Task1.1_PUSH/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1._1_PUSH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name: ");
+            string X = Console.ReadLine();
+            Console.WriteLine("Hello, " + X + " This Task is just to push to GitHub in my own self learning");
+            Console.WriteLine("Journey! Thank me for believing in myself! (0)J(0) ");
+
+            /* 🫡🫡🫡🫡🫡 */
+        }
+    }
+}

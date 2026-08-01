4c4af99f36124dc103606e472634eac48177fa59
hamza <adawihsa@gmail.com>
2026-08-01 05:40:23 +0400

    For loop practice done

---
+++ Task1_Practice2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApplication
+{
+    // This is a for loop that will continue to execute as
+    // long as i is less than or equal to 100
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a Number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = x; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // in for loop we can use the same variable
+            // name as the input variable name but it will be a different variable
+            // always best practice is to use different variable names to avoid confusion
+
+        }
+    }
+}

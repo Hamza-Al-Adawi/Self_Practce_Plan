e84a8a3cf2ae85d10d835dc6016d3542bb1d9aba
hamza <adawihsa@gmail.com>
2026-08-01 05:32:05 +0400

    Added  Comments

---
+++ Task_1_Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number less than 10: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int x = i;
+
+            // This is a while loop that will continue to execute as long as x is less than 10
+
+            // use x instead of i because i is the input number and
+            // x is the variable that will be incremented
+
+            while (x < 10)
+            {
+                x++; // increment x by 1
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
